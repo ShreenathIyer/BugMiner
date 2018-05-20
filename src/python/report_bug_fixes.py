3e75a2453fad9cea5ada87f5b256d4036e5799b7
Ashwin Bhandare <ashwin@campus-046-110.ucdavis.edu>
2018-05-20 14:30:12 -0700

    findIssueType

--- src/python/report_bug_fixes.py
+++ src/python/report_bug_fixes.py
@@ -1,16 +1,40 @@
 import os
 import json
 import re
+from bs4 import BeautifulSoup
+import urllib.request as urllib2
 
 class ReportBugFixes(object):
     def __init__(self):
         self.json_path = os.path.abspath("../../resources/commits.json")
         self.data = self.parse_json()
-        # self.keyword = "fix"
         self.commit_couples = [{"count": 0, "commit_pairs": {}}]
-        # self.couple_half = False
-        self.need_next = False
-        self.prev = 0
+
+    def is_issue_bug(self, url):
+        response = urllib2.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, "lxml")
+
+        file1 = open('out2.txt', 'w')
+        file1.write(str(soup))
+        file1.close()
+
+        file1 = open('out2.txt')
+        term = "title=\"Bug"
+        file2 = open('out3.txt', 'a')
+
+        for line in file1:
+            line.strip().split('/n')
+            if term in line:
+                myTitle = soup.html.head.title
+                file2.write(str(myTitle.contents))
+                file2.write("\n")
+                break
+
+        file1.close()
+        file2.close()
+
+
 
     def parse_json(self):
         json_file = open(self.json_path, 'r', encoding='utf-8')
@@ -19,9 +43,6 @@ class ReportBugFixes(object):
     def create_commit_pairs(self):
         for commit_history in self.data["items"]:
             project_name = commit_history["repository"]["name"].lower()
-            if self.need_next:
-                self.commit_couples[0]["commit_pairs"][self.prev].append(commit_history["sha"])
-                self.need_next = False
 
             if project_name in commit_history["commit"]["message"].lower():
                 jira_id = re.search(r"\w*"+project_name+"-\w*", commit_history["commit"]["message"].lower())
@@ -29,18 +50,18 @@ class ReportBugFixes(object):
                     self.commit_couples[0]["count"] += 1
                     self.commit_couples[0]["commit_pairs"][jira_id.group().upper()] = list()
                     self.commit_couples[0]["commit_pairs"][jira_id.group().upper()].append(commit_history["sha"])
-                    self.need_next = True
+                    self.commit_couples[0]["commit_pairs"][jira_id.group().upper()].append(commit_history["parents"][0]["sha"])
                     self.prev = jira_id.group().upper()
-        # for commit_history in self.data["items"]:
-        #     if re.search(r"\bfix\b|\bfixed\b", commit_history["commit"]["message"].lower()):
-        #         self.commit_couples.append(commit_history["sha"].split())
-        #         self.couple_half = True
-        #     elif self.couple_half:
-        #         self.commit_couples[(len(self.commit_couples) - 1)].append(commit_history["sha"])
-        #         self.couple_half = False
+
+                    term = jira_id.group().upper()
+                    url = 'https://issues.apache.org/jira/browse/' + term
+                    self.is_issue_bug(url)
         return self.commit_couples
+
+
 
 
 if __name__ == "__main__":
     report = ReportBugFixes()
     print(report.create_commit_pairs())
+    print()
