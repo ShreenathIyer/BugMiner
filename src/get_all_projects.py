54c9405f592f577a5be175cb0c5018e53a63ceca
Ashwin Bhandare <ashwin@ashwins-mbp.attlocal.net>
2018-06-08 11:58:31 -0700

    Added Jira URl to projects and created a dictionary

--- src/get_all_projects.py
+++ src/get_all_projects.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import csv 
 
 class GetAllProjects:
     def __init__(self):
         pass
 
     def read_projects_from_csv(self):
-        df = pd.read_csv("../resources/projects.csv")
-        return df['Jira Name of the project'].tolist()
+        reader = csv.DictReader(open('../resources/projects.csv'))
+        projects = {}
+        for row in reader:
+            projects[row['Jira Name of the project']] = row['JiraURL']
+        return projects 
+
+        #print(result)
+        #df = pd.read_csv("../resources/projects.csv")
+        #print("df is" + df)
+        #return df['Jira Name of the project'].tolist()
 
 
 if __name__ == "__main__":
-    # obj = GetAllProjects()
-    # print(obj.read_projects_from_csv())
-    pass
+     obj = GetAllProjects()
+     print(obj.read_projects_from_csv())
+     pass
